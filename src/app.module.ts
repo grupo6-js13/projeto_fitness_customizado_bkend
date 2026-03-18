45312ca597cd616b48cb60daba6e1f7524f6268b
Bfr-JHon <jhonathavinicius21@gmail.com>
2026-03-18 18:18:05 -0300

    realizando deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,26 +4,24 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { ExercicioModule } from './exercicio/exercicio.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localHost",
-      port: 3307, //  Meu MySQL está rodando na porta 3307. Favor ajustar conforme necessário
-      username: "root", // O usuário padrão do MySQL é "root". Favor ajustar conforme necessário
-      password: "root", // A senha padrão do MySQL é "root". Favor ajustar conforme necessário
-      database: "db_fitness", // Crie um banco de dados chamado "db_fitness" no MySQL      
-      autoLoadEntities: true, //Procurar por arquivos de forma recursiva
-      synchronize: true,
-      dropSchema: false, // Cuidado: isso irá apagar o banco de dados a cada reinício da aplicação. Use apenas em desenvolvimento.
-    }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+   }),
+   
     UsuarioModule,
     CategoriaModule,
     ExercicioModule,
     AuthModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
---
+++ src/data/services/dev.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+import { Usuario } from "../../usuario/entities/usuario.entity";
+import { Exercicio } from "../../exercicio/entities/exercicio.entity";
+import { Categoria } from "../../categoria/entities/categoria.entity";
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'db_fitness',
+            entities: [Exercicio, Categoria, Usuario],
+            synchronize: true,
+    };
+  }
+}
