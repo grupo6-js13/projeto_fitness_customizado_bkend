8b21a647d4f26c00b997756b62a40e5de1571863
Josue-Bravo <josuebc8@gmail.com>
2026-03-18 12:01:20 -0300

    🎉 Initial Commit: Configuração base dos arquivos e bibliotecas

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localHost",
+      port: 3307, // Meu MySQL está rodando na porta 3307. Favor ajustar conforme necessário
+      username: "root", // O usuário padrão do MySQL é "root". Favor ajustar conforme necessário
+      password: "root", // A senha padrão do MySQL é "root". Favor ajustar conforme necessário
+      database: "db_fitness", // Crie um banco de dados chamado "db_farmacia" no MySQL
+      autoLoadEntities: true, //Procurar por arquivos de forma recursiva
+      synchronize: true,
+      dropSchema: false, // Cuidado: isso irá apagar o banco de dados a cada reinício da aplicação. Use apenas em desenvolvimento.
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  process.env.TZ = '-03:00';
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Fitness Personalizado')
+    .setDescription('Documentação da API de Fitness Personalizado')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  // Redirecionar a raiz para o Swagger
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.redirect('/swagger');
+  });
+
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
